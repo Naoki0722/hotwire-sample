bbdb25dca21677a346a6dddb9fb99d8ed548bbad
Naoki0722 <mattu.nao722@gmail.com>
2024-06-10 22:54:11 +0900

    Finish

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,8 @@
 class Cat < ApplicationRecord
 
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   class << self
     def ransackable_attributes(_auth_object = nil)
       %w[name age]
